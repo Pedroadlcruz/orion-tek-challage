2079a00c03d36669e0775c4ce5220772c15fc6ae
Pedro A. De la Cruz <pedrodlcruz@coolhunterpartners.com>
2023-10-31 21:21:48 -0400

    💄 Add CompanyCard widget

--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -1,3 +1,4 @@
 class Strings {
   static const appName = 'Orion App';
+  static const companies = 'Empresas';
 }
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -6,8 +6,66 @@ class HomeScreen extends StatelessWidget {
   static const String routeName = '/';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text(Strings.appName)),
+    return Scaffold(
+      appBar: AppBar(title: const Text(Strings.companies)),
+      body: ListView(
+        children: const [
+          CompanyCard(
+              name: Strings.appName,
+              logo:
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtH6ctIDoPfhmlQreh9wC8fy65XzroD6O5Xg&usqp=CAU"),
+        ],
+      ),
+    );
+  }
+}
+
+class CompanyCard extends StatelessWidget {
+  final String name;
+  final String logo;
+  final void Function()? onTap;
+
+  const CompanyCard({
+    super.key,
+    required this.name,
+    required this.logo,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 3,
+      margin: const EdgeInsets.all(16),
+      child: InkWell(
+        onTap: onTap,
+        child: Column(
+          children: <Widget>[
+            Image.network(
+              logo,
+              width: double.infinity,
+              height: 150,
+              fit: BoxFit.fill,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    name,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
